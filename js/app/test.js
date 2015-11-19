9d5553cb03b9cc3c3a97685fbb301627cc002d0d
Royee <royeeee@outlook.com>
2015-11-19 23:02:49 +0800

    update

--- js/app/test.js
+++ js/app/test.js
@@ -1,6 +1,9 @@
 define(["../jquery","ctrl/templateController","ctrl/dataController"], function($,_t) {
-		
-		var res = _t.bind($('#template-container'),'pan',{name:"tom"},function(){
+		var obj = $('#template-container'),
+		    template = 'pan',
+		    data = ''
+
+		var res = _t.bind(obj,template,data,function(){
 
 		});
 
