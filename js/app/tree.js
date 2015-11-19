46d0ceeb39e3c0bb3fca52a36451740f1bdeaf1b
Royee <royeeee@outlook.com>
2015-11-19 23:01:48 +0800

    update with plugin tree

--- js/app/tree.js
+++ js/app/tree.js
@@ -3,7 +3,8 @@ define(["../jquery","../tree","../setting"], function($,t,cfg) {
 		var res = cfg.getConfig("treeResource");
 
         $('.tree').TDMTree({
-            resource:res
+            resource:res,
+            preventLink:true
         });
        
     });
